0ab5008952535dc0a35149f76c4e85729f90a7a6
JCKalt <ckaltwasser@reminingcorp.com>
2024-05-10 12:14:29 -0400

    adding hourly data

--- sql/updates/upd-001.sql
+++ sql/updates/upd-001.sql
@@ -1,17 +1,4 @@
--- Create the schema
-CREATE SCHEMA IF NOT EXISTS weather;
--- Set the schema
 SET search_path TO weather;
-
--- Create the weather.weather_data table
-CREATE TABLE weather_data (
-    date DATE NOT NULL,
-    address VARCHAR(255) NOT NULL,
-    temp DECIMAL(5,2),
-    tempmin DECIMAL(5,2),
-    tempmax DECIMAL(5,2),
-    PRIMARY KEY (date, address)
-);
 
 -- Create the weather.hourly_data table
 CREATE TABLE IF NOT EXISTS weather.hourly_data (
