45434ea8ead60e78a1b1e086c4195ea4ef19c434
Chris Kaltwasser <ckaltwasser@plasmaguardllc.com>
2024-05-10 15:46:25 +0000

    wea

--- bin/get-weather-data.sh
+++ bin/get-weather-data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python src/get-weather-data.py --start-date "2023-11-01" --end-date "2023-12-17" --address "Scottsdale, AZ"
+python src/get-weather-data.py --start-date "2022-10-01" --end-date "2024-03-31" --address "Las Vegas, NV"
 
